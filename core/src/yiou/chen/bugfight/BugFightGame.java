b57114882d5f95384b9325a89f8186527a5c4030
Yiou Chen <yiou.chen1992@gmail.com>
2014-11-08 19:47:56 -0500

    test commit

--- core/src/yiou/chen/bugfight/BugFightGame.java
+++ core/src/yiou/chen/bugfight/BugFightGame.java
@@ -5,7 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+//test change
+//test change2
 public class BugFightGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
