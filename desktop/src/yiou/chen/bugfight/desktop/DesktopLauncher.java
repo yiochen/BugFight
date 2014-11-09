7bac2cdaaad7cc0cfa512db7132d9086f55cbca8
Yiou <yiou.chen1992@gmail.com>
2014-11-08 19:31:41 -0500

    Initial commit

---
+++ desktop/src/yiou/chen/bugfight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package yiou.chen.bugfight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import yiou.chen.bugfight.BugFightGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BugFightGame(), config);
+	}
+}
